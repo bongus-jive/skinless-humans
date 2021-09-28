0863efb3ec8b09b36a3735bfc1178a9174be7fa0
patman <27874300+patmanf@users.noreply.github.com>
2021-09-28 14:19:00 -0500

    use generic player script instead of hooking into companion scripts

--- pat/noskin/noskinCheck.lua
+++ pat/noskin/noskinCheck.lua
@@ -1,8 +1,4 @@
-local updatebutsexy = update
-
 function update(dt)
-	updatebutsexy(dt)
-	
 	if player.species() == "human" and status.statusProperty("pat_noskin", 0) == 0 then
 		status.addEphemeralEffect("pat_noskin", math.huge)
 	end
