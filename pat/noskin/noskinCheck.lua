61d9e799906a3be97c3e360f871aa4c0d22047c5
patman <27874300+patmanf@users.noreply.github.com>
2025-02-10 18:18:17 -0600

    use a species status effect instead of  whatever the hell i was doing before

--- pat/noskin/noskinCheck.lua
+++
@@ -1,5 +0,0 @@
-function update(dt)
-	if player.species() == "human" and status.statusProperty("pat_noskin", 0) == 0 then
-		status.addEphemeralEffect("pat_noskin", math.huge)
-	end
-end
--- pat/noskin/reset.lua
+++
@@ -1,7 +0,0 @@
-function activate(fireMode, shiftHeld)
-	if fireMode == "primary" then
-		status.setStatusProperty("pat_noskin", 1)
-	elseif fireMode == "alt" then
-		status.setStatusProperty("pat_noskin", 0)
-	end
-end
