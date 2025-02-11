3505f8d6ba55db55268f52999c53b8395a5e72a9
patman <27874300+patmanf@users.noreply.github.com>
2025-02-10 22:03:51 -0600

    randomize sound pitch

--- pat/noskin/noskin.lua
+++ pat/noskin/noskin.lua
@@ -3,11 +3,17 @@ function init()
   animator.playSound("agony", -1)
 
   self.healthDrain = config.getParameter("healthDrain")
+  self.pitchRange = config.getParameter("pitchRange")
+end
+
+local function randomInRange(range)
+  return range[1] + (math.random() * (range[2] - range[1]))
 end
 
 function update(dt)
   animator.setParticleEmitterOffsetRegion("blood", mcontroller.boundBox())
-  
+  animator.setSoundPitch("agony", randomInRange(self.pitchRange))
+
   status.overConsumeResource("health", self.healthDrain * dt)
 end
 
